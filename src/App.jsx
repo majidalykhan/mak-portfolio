c42353b49c535af58573e8a895c13dd37d0f5c39
majidalykhan <majidalykhan@gmail.com>
2022-09-01 18:01:19 +0500

    Create all components

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+import {
+  Header,
+  Nav,
+  About,
+  Experience,
+  Services,
+  Portfolio,
+  Testimonials,
+  Contact,
+  Footer,
+} from "./components";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
