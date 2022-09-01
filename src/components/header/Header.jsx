c8f9d71e20668503f84b5efb7a10e37ff18138a9
majidalykhan <majidalykhan@gmail.com>
2022-09-02 01:59:05 +0500

    Header style modifications

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,25 +3,18 @@ import "./header.css";
 
 import CTA from "./CTA.jsx";
 import HeaderSocials from "./HeaderSocials";
-import { images } from "../../constants";
 
 const Header = () => {
   return (
     <header>
       <div className="container header__container">
-        <h5>Hello I'm</h5>
-        <h1>Majid Ali Khan</h1>
-        <h5 className="text-dark">Developer</h5>
+        <div class="content">
+          <h2>Majid Ali Khan</h2>
+          <h2>Majid Ali Khan</h2>
+        </div>
+        <p>Software Engineer</p>
         <CTA />
         <HeaderSocials />
-
-        <div className="me">
-          <img src={images.profileImage} alt="me"></img>
-        </div>
-
-        {/* <a href="#contact" className="scroll__down">
-          Scroll Down
-        </a> */}
       </div>
     </header>
   );
