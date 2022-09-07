3d8961e84223734c3647badb26479177431f634e
Majid Ali Khan <majidalykhan@gmail.com>
2022-09-08 02:20:12 +0500

    Contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,8 +1,63 @@
 import React from "react";
 import "./contact.css";
 
+import { AiOutlineMail, AiOutlineWhatsApp } from "react-icons/ai";
+import { SiFiverr } from "react-icons/si";
+
 const Contact = () => {
-  return <section id="contact">Contact</section>;
+  return (
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <AiOutlineMail className="contact__option-icon" />
+            <h4>Email</h4>
+            <h5>majialykhan@gmail.com</h5>
+            <a href="mailto:majidalykhan@gmail.com" target="_blank">
+              Send a message
+            </a>
+          </article>
+          <article className="contact__option">
+            <SiFiverr className="contact__option-icon" />
+            <h4>Fiverr</h4>
+            <h5>majialykhan</h5>
+            <a href="https://www.fiverr.com/majidalykhan" target="_blank">
+              Send a message
+            </a>
+          </article>
+          <article className="contact__option">
+            <AiOutlineWhatsApp className="contact__option-icon" />
+            <h4>Whatsapp</h4>
+            <h5>Majid Ali Khan</h5>
+            <a
+              href="https:/api.whatsapp.com/send?phone=03105484207"
+              target="_blank"
+            >
+              Send a message
+            </a>
+          </article>
+        </div>
+        {/* End of Contact Options */}
+        <form action="">
+          {/* Form submission to set up later */}
+          <input type="text" name="name" placeholder="Your Name" required />
+          <input type="email" name="email" placeholder="Your Email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your Message"
+            required
+          ></textarea>
+          <button type="submit" className="btn">
+            Send Message
+          </button>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
